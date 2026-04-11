38a6a06e631dad18499934b2f3fd40aa6f969d95
Junior <139707140+Juniorborgesdapaz@users.noreply.github.com>
2026-04-11 16:22:32 -0300

    .

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-function executar(taskname, callback){
-    console.log("teste da funcçaõ", taskname)
-    callback()
-    
-}
-function callback(){
-    console.log("tarefa finalizada")
-}
-executar("teste", callback)
