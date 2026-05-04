20679ac1b2617dde557f78b31111b4ac1df3ba79
Junior <139707140+Juniorborgesdapaz@users.noreply.github.com>
2026-05-04 13:53:03 -0300

    m

---
+++ javascript/Aula_1/script.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('input')
+const form = document.querySelector('button')
+
+input.onsubmit = (event) => {
+    event.preventDefault()
+}
+/*
+    const value = input.value
+    const regex = /\d+/
+
+    if (regex.test(value)) {
+        alert("O texto só aceita letras, remova os números")
+    } else {
+        alert("Enviado")
+    }
+}
+    
+*/
+
+alert('teste')
